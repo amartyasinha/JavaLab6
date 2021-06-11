b06fdcffccf002ff57f7143133ba29d4e8f5cd10
amartyasinha918 <amartyasinha918@gmail.com>
2021-06-12 01:40:36 +0530

    JavaLab6 Commit

--- src/CopyContentFileHandling.java
+++ src/CopyContentFileHandling.java
@@ -1,26 +1,28 @@
 import java.util.Scanner;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class CopyContentFileHandling {
     public static void main(String[] args) {
-        File file1 = new File(args[0]);
-
+        if (args.length != 2) {
+            System.out.println("Not enough Arguments (Filename)\nClosing Program...");
+            System.exit(101);
+        }
         try {
-            FileWriter fileWrite = new FileWriter(args[1]);
-            Scanner sc = new Scanner(file1);
+            File fin = new File(args[0]);
+            FileWriter fout = new FileWriter(args[1]);
+            Scanner sc = new Scanner(fin);
 
             while (sc.hasNextLine()) {
-                System.out.println("Reading Content of Source File");
-                fileWrite.write(sc.nextLine());
+                System.out.println("Reading Content of Source File\n.\n.\n.");
+                fout.write(sc.nextLine());
                 System.out.println("Completed Reading text of Source File");
             }
+            fout.close();
 
-            fileWrite.close();
-
+        } catch (FileNotFoundException e) {
+            System.out.println("Not able to open the File!" + e);
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Not able to Read/Write the File(s)!" + e);
         }
     }
 }
